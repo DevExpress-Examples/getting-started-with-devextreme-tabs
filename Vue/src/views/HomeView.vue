d6f480eb6ff85f1b2ac08a1b91226d9d4424b9f7
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-10-09 17:36:42 +0300

    Update repository: remove orig files, rename HomeContent to TabsContent, update README with jQuery-first order

--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import HomeContent from '../components/HomeContent.vue';
+import TabsContent from '../components/TabsContent.vue';
 </script>
 
 <template>
-  <HomeContent text="Count"/>
+  <TabsContent text="Count"/>
 </template>
 
