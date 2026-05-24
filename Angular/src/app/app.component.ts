1f89bb0977bd5c951d04cbb8f22ca926292d4be7
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:10:34 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import notify from 'devextreme/ui/notify';
 import type { ItemClickEvent } from 'devextreme/ui/tabs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   onItemClick(e: ItemClickEvent): void {
